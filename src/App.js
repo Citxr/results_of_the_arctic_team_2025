d53192c4a1534f7540256e000118f30877070062
hrtlock <dsrubc@gmail.com>
2024-12-03 01:25:49 +0300

    work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./App.css";
+
+const images = [
+    "img/11.png",
+    "img/12.png",
+    "img/13.png",
+    "img/14.png",
+    "img/15.png",
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const sliderRef = useRef(null);
+
+    const handleShare = (src) => {
+        if (navigator.share) {
+            navigator.share({
+                title: "Поделиться изображением",
+                text: "Посмотрите это изображение!",
+                url: src, // Ссылка на изображение
+            })
+                .then(() => console.log("Успешно поделились"))
+                .catch((error) => console.error("Ошибка при попытке поделиться", error));
+        } else {
+            alert("Ваше устройство не поддерживает функцию поделиться.");
+        }
+    };
+
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
+    return (
+        <div className="slider-container">
+            {/* Кнопка "влево" */}
+            <button
+                className="arrow-button left"
+                onClick={() => sliderRef.current.slickPrev()}
+            >
+                &lt;
+            </button>
+
+            <Slider ref={sliderRef} {...settings} className="slider">
+                {images.map((src, index) => (
+                    <div key={index} className="slide">
+                        <img src={src} alt={`Slide ${index + 1}`} className="slide-image" />
+                        {index !== 0 && index !== images.length - 1 && (
+                            <button
+                                className="share-button"
+                                onClick={() => handleShare(src)}
+                            >
+                                Поделиться
+                            </button>
+                        )}
+                    </div>
+                ))}
+            </Slider>
+
+            {/* Кнопка "вправо" */}
+            <button
+                className="arrow-button right"
+                onClick={() => sliderRef.current.slickNext()}
+            >
+                &gt;
+            </button>
+        </div>
+    );
 }
 
 export default App;
