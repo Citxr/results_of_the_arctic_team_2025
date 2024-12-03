08ab9fd73588aa6c659b3d0db761380bd94edba2
imilyya <ilyasovem@yandex.ru>
2024-12-03 18:13:52 +0300

    feat: connected vk bridge & vk tunnel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./App.css";
+import bridge from "@vkontakte/vk-bridge";
 
 const images = [
     "img/11.png",
